6fdd4a60eb45b99ff39c947f012bc1079e6a8611
muukvds <muukvds@users.noreply.github.com>
2017-09-26 17:28:04 +0200

    create Cake class and constructor

---
+++ src/Cake.java
@@ -0,0 +1,10 @@
+public class Cake {
+    private String name;
+    private boolean sugarFree;
+
+    public Cake(String name, boolean sugarFree) {
+        this.name = name;
+        this.sugarFree = sugarFree;
+    }
+    
+}
