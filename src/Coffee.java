c6361a859c7fe724c784508c5b23e6f6233e6e06
muukvds <muukvds@users.noreply.github.com>
2017-09-26 17:30:10 +0200

    create Coffee class and constructor

---
+++ src/Coffee.java
@@ -0,0 +1,10 @@
+public class Coffee {
+
+    private String name;
+    private int size;
+
+    public Coffee(String name, int size) {
+        this.name = name;
+        this.size = size;
+    }
+}
